145f71c5fe7f0e8341870e4bb4d96788218943e8
Yusuke Hayashi <yusuke8h@gmail.com>
2026-07-28 05:37:36 +0900

    Release Hayate 0.15.0 (#62)

--- src/hayate/__init__.py
+++ src/hayate/__init__.py
@@ -17,7 +17,7 @@ from .urlpattern import URLPattern, URLPatternResult
 from .validator import validator
 from .websocket import WebSocket, WebSocketClosed
 
-__version__ = "0.14.0"
+__version__ = "0.15.0"
 
 __all__ = [
     "URL",
